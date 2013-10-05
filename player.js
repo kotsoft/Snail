87fab6ae2fa97f6376450a00665ec9af0da43cda
Roly Fentanes <roly426@gmail.com>
2013-10-05 11:51:27 -0400

    drawing a circle

--- player.js
+++ player.js
@@ -1,11 +1,13 @@
-SNAIL.player = {
+var PI2 = Math.PI * 2;
+
+sNAIL.player = {
   init: function(x, y) {
     this.x = this.xprev = x;
     this.y = this.yprev = y;
   },
   update: function(dt){
     var dx = this.x - this.xprev;
-    var dy = this.y - this.yprev + .1 * dt*dt;
+    var dy = this.y - this.yprev + 0.1 * dt*dt;
     this.xprev = this.x;
     this.yprev = this.y;
     this.x += dx;
@@ -13,5 +15,19 @@ SNAIL.player = {
     var newPos = SNAIL.staticCollision(this.x, this.y);
     this.x = newPos[0];
     this.y = newPos[1];
-  }
+  },
+  draw: function() {
+    var ctx = SNAIL.ctx;
+    var radius = ~~(SNAIL.blockWidth / 2);
+    var centerX = this.x + radius;
+    var centerY = this.y + radius;
+    ctx.beginPath();
+    ctx.arc(centerX, centerY, radius, 0, PI2);
+    ctx.fillStyle = 'rgba()';
+    ctx.fill();
+    ctx.lineWidth = 3;
+    ctx.strokeStyle = '#030';
+    ctx.stroke();
+    ctx.closePath();
+  },
 }
